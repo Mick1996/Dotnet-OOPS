d9c000b658e9b163d4718a24a704525d17df3502
Mick1996# <mickykumar51@gmail.com>
2022-03-29 13:30:43 +0530

    refit producer

---
+++ Refit Implementaion/Model/GuestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Refit_Implementaion.Model
+{
+    public class GuestModel
+    {
+        public int Id { get; set; } 
+        public string FirstName { get; set;}
+        public string LastName { get; set;} 
+    }
+}
